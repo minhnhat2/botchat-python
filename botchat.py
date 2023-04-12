f30609d5da0b863e9989c76369420b08690b2c0e
Lê Minh Nhật <111627866+minhnhat2002@users.noreply.github.com>
2023-04-13 06:37:09 +0700

    Update botchat.py

--- botchat.py
+++ botchat.py
@@ -1,17 +1,59 @@
-import nltk
-from nltk.chat.util import Chat, reflections
+import tkinter as tk
+from datetime import datetime
+import webbrowser
+import os
 
-pairs = [
-    ["my name is (.*)", ["hi %1"]],
-    ["hi|hello|hola", ["hello there!", "hi there!"]], 
-    ["(.*) age?", ["I am a computer program, I don't have age"]],
-    ["(.*) (location|city)", ["I am located in a virtual space"]],
-    ["(.*) created?", ["I was created by a team of developers"]],
-    ["how (.*) health(.*)", ["I am a computer program, I don't have health"]],
-    ["(.*) (sports|game)", ["I am not interested in sports or games"]],
-    ["(.*) (movies|films)", ["I love movies! Which is your favorite?"]],
-    ["(.*) (book|novel)", ["I love reading books! What's your favorite book?"]]
-]
+keywords = {}
+with open('keywords.txt', 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+    for line in lines:
+        keyword, answer = line.strip().split('|')
+        keywords[keyword] = answer
 
-chatbot = Chat(pairs, reflections)
-chatbot.converse()
+root = tk.Tk()
+root.title("Chat Bot")
+root.geometry("400x400")
+
+input_box = tk.Entry(root, width=40)
+input_box.pack(pady=10)
+
+chat_box = tk.Text(root, width=40, height=15)
+chat_box.pack()
+
+def get_time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
+
+def handle_input():
+    user_input = input_box.get()
+    chat_box.insert(tk.END, f"\nYou: {user_input}")
+    input_box.delete(0, tk.END)
+
+    if user_input.lower() == "shutdown":
+        os.system("shutdown /s /t 1")
+        chat_box.insert(tk.END, f"\nBot: Shutting down the computer...")
+    elif user_input.lower() == "open google":
+        webbrowser.open("https://www.google.com")
+        chat_box.insert(tk.END, f"\nBot: Opening Google...")
+    elif "time" in user_input.lower():
+        current_time = get_time()
+        chat_box.insert(tk.END, f"\nBot: The current time is {current_time}")
+    else:
+        found_keyword = False
+        for keyword in keywords:
+            if keyword in user_input.lower():
+                chat_box.insert(tk.END, f"\nBot: {keywords[keyword]}")
+                found_keyword = True
+                break
+        if not found_keyword:
+            webbrowser.open(f"https://www.google.com/search?q={user_input}")
+            chat_box.insert(tk.END, f"\nBot: I'm sorry, I don't have an answer for that. "
+                                     f"I've searched it on Google for you.")
+
+    chat_box.see(tk.END)
+
+submit_button = tk.Button(root, text="Submit", command=handle_input)
+submit_button.pack()
+
+root.mainloop()
